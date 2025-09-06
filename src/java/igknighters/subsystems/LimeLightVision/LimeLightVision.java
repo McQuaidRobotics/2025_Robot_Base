0b13f2594e55865a8e6a2f9c44ff2f799724d483
Henry Winchester <henryj.winchester@outlook.com>
2025-09-06 12:09:46 -0400

    started rot fix

--- src/java/igknighters/subsystems/LimeLightVision/LimeLightVision.java
+++ src/java/igknighters/subsystems/LimeLightVision/LimeLightVision.java
@@ -3,6 +3,7 @@ package igknighters.subsystems.LimeLightVision;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import igknighters.LimelightHelpers;
 import igknighters.subsystems.Subsystems;
 import java.util.ArrayList;
@@ -23,17 +24,21 @@ public class LimeLightVision implements Subsystems.SharedSubsystem {
     List<Pose2d> poses = new ArrayList<>();
     double timestamp = 0.0;
     for (String cameraName : cameraNames) {
-      LimelightHelpers.SetRobotOrientation(
-          cameraName, rotationOfRobotInDegrees, 0.0, 0.0, 0.0, 0.0, 0.0);
-      var llMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
+
+      var llMeasurement =
+          DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue)
+                  == DriverStation.Alliance.Red
+              ? LimelightHelpers.getBotPoseEstimate_wpiRed_MegaTag2(cameraName)
+              : LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
 
       if (llMeasurement != null && llMeasurement.tagCount > 0) {
+        DogLog.log("Robot/Subsystems/LimeLightVision/RawPose_" + cameraName, llMeasurement.pose);
         poses.add(llMeasurement.pose);
         timestamp += llMeasurement.timestampSeconds;
       }
     }
     if (!cameraNames.isEmpty()) {
-      timestamp /= cameraNames.size();
+      timestamp /= poses.size();
     }
     lastTimeStamp = timestamp;
     DogLog.log("Robot/Subsystems/LimeLightVision/TimeStampOfMeasurments", timestamp);
@@ -52,19 +57,22 @@ public class LimeLightVision implements Subsystems.SharedSubsystem {
 
     double xSum = 0.0, ySum = 0.0;
     double sinSum = 0.0, cosSum = 0.0;
+    List<Double> rotations = new ArrayList<>();
 
     for (Pose2d pose : poses) {
       xSum += pose.getX();
       ySum += pose.getY();
       sinSum += Math.sin(pose.getRotation().getRadians());
       cosSum += Math.cos(pose.getRotation().getRadians());
+      rotations.add(pose.getRotation().getDegrees());
     }
 
     int count = poses.size();
     double avgX = xSum / count;
     double avgY = ySum / count;
+    DogLog.log("Robot/Subsystems/LimeLightVision/RotationList", rotations.toString());
     Rotation2d avgRot = new Rotation2d(Math.atan2(sinSum / count, cosSum / count));
-
+    DogLog.log("Robot/Subsystems/LimeLightVision/Rotation", avgRot.getDegrees());
     Pose2d averaged = new Pose2d(avgX, avgY, avgRot);
     DogLog.log("Robot/Subsystems/LimeLightVision/TagsSeen", averaged);
     return averaged;
