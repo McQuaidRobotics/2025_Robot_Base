ad5d667af0a62ce57e0ee465295e1180877f8c88
Henry <henryj.winchester@outlook.com>
2025-09-20 21:16:06 -0400

    added file not much else

--- src/java/igknighters/Robot.java
+++ src/java/igknighters/Robot.java
@@ -6,12 +6,18 @@ package igknighters;
 
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
+import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.StringSubscriber;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import igknighters.commands.autos.AutoRoutines;
 import igknighters.commands.teleop.TeleopSwerveWithDetune;
 import igknighters.controllers.DriverController;
 import igknighters.subsystems.LimeLightVision.Helpers.LimelightVisionConstants;
@@ -19,10 +25,14 @@ import igknighters.subsystems.LimeLightVision.LimeLightVisionReal;
 import igknighters.subsystems.LimeLightVision.LimeLightVisionSim;
 import igknighters.subsystems.Subsystems;
 import igknighters.subsystems.swerve.generated.knightshadeConsts;
+import monologue.LogSink;
+import monologue.Monologue;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private final AutoFactory autoFactory;
+  public final AutoChooser autoChooser = new AutoChooser();
+  private final CommandScheduler scheduler = CommandScheduler.getInstance();
 
   private final DriverController driverController = new DriverController(0);
 
@@ -45,20 +55,17 @@ public class Robot extends TimedRobot {
     subsytems.swerve.registerTelemetry(logger::telemeterize);
     driverController.bind(subsytems);
     autoFactory = subsytems.swerve.createAutoFactory();
+    final var routines = new AutoRoutines(subsytems, autoFactory);
+    AutoRoutines.addCmd(autoChooser, "tested", routines::driveAround);
+    autoChooser.addCmd("TRAJECTORY TEST", routines.trajTest("Straight"));
+    SmartDashboard.putData("AUTO CHOOSER", autoChooser);
+    // RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
 
-    /*
-     * This example of adding Limelight is very simple and may not be sufficient for on-field use.
-     * Users typically need to provide a standard deviation that scales with the distance to target
-     * and changes with number of tags available.
-     *
-     * This example is sufficient to show that vision integration is possible, though exact implementation
-     * of how to use vision should be tuned per-robot and to the team's specification.
-     */
     if (kUseLimelight) {
       var driveState = subsytems.swerve.getState();
       double headingDeg = driveState.Pose.getRotation().getDegrees();
@@ -72,7 +79,9 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    scheduler.cancelAll();
+  }
 
   @Override
   public void disabledPeriodic() {}
@@ -81,13 +90,21 @@ public class Robot extends TimedRobot {
   public void disabledExit() {}
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    Command autoCmd = autoChooser.selectedCommand();
+    String msg = "---- Starting auto command: " + autoCmd.getName() + " ----";
+    if (false) System.out.println(msg);
+    Monologue.log("AutoEvent", msg);
+    scheduler.schedule(autoCmd);
+  }
 
   @Override
   public void autonomousPeriodic() {}
 
   @Override
-  public void autonomousExit() {}
+  public void autonomousExit() {
+    scheduler.cancelAll();
+  }
 
   @Override
   public void teleopInit() {
@@ -115,4 +132,27 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {}
+
+  private void setupAutoChooser() {
+    Monologue.publishSendable("/Choosers/AutoChooser", autoChooser, LogSink.NT);
+    final StringSubscriber sub =
+        NetworkTableInstance.getDefault()
+            .getStringTopic("/Choosers/AutoChooser/selected")
+            .subscribe(
+                "",
+                PubSubOption.pollStorage(1),
+                PubSubOption.periodic(0.5),
+                PubSubOption.sendAll(true),
+                PubSubOption.keepDuplicates(false));
+    this.addPeriodic(
+        () -> {
+          var queue = sub.readQueueValues();
+          if (queue.length > 0) {
+            System.out.println("AutoChooser selected: " + queue[0]);
+            autoChooser.select(queue[0]);
+          }
+        },
+        kDefaultPeriod,
+        0.01);
+  }
 }
